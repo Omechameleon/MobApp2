3012fe8d17fafa0dd14265e239e18af1031737f1
MaxH <heylenmaxim@gmail.com>
2018-01-21 12:42:31 +0100

    fix

--- TeacherReacherMobileApp/app/src/main/java/com/example/heylen/teacherreachermobileapp/LoginActivity.java
+++ TeacherReacherMobileApp/app/src/main/java/com/example/heylen/teacherreachermobileapp/LoginActivity.java
@@ -129,8 +129,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onBackPressed(){
-    finish();
-    moveTaskToBack(true);
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
 
